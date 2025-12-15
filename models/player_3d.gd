6bbda79d6b816a1f97d88d7c05953514abd1bb4b
AceLazer <gsw.jam.pg30@gmail.com>
2025-12-15 11:53:32 -0700

    Gravity!
    We have gravity now, plus a jump impulse, and gravity variable. Velocity.y is now being calculated

--- models/player_3d.gd
+++ models/player_3d.gd
@@ -7,10 +7,12 @@ extends CharacterBody3D
 @export var move_speed := 8.0
 @export var acceleration := 20.0
 @export var rotation_speed := 12.0
+@export var jump_impulse := 12.0
 
 
 var _camera_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.BACK
+var _gravity := -30.0 
 
 @onready var _camera_pivot: Node3D = %CameraPivot
 @onready var _camera: Camera3D = %Camera3D
@@ -49,10 +51,17 @@ func _physics_process(delta: float) -> void:
 	move_direction.y = 0.0
 	move_direction = move_direction.normalized()
 	
+#	storing velocities
+	var y_velocity := velocity.y
+	velocity.y = 0.0
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	velocity.y = y_velocity + _gravity * delta
+	
 	move_and_slide()
 	
 	if move_direction.length() > 0.2:
 		_last_movement_direction = move_direction
 	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
 	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
+	
+	
