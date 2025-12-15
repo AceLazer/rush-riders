b7661d70da5b6b784c21a78710fdd486aa8ba79f
AceLazer <gsw.jam.pg30@gmail.com>
2025-12-15 11:26:18 -0700

    Character Y axis rotation but smooth
    Rotation has been smoothed out thanks to the lerp_angle

--- models/player_3d.gd
+++ models/player_3d.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 @export_group("Movement")
 @export var move_speed := 8.0
 @export var acceleration := 20.0
-
+@export var rotation_speed := 12.0
 
 
 var _camera_input_direction := Vector2.ZERO
@@ -15,6 +15,7 @@ var _last_movement_direction := Vector3.BACK
 @onready var _camera_pivot: Node3D = %CameraPivot
 @onready var _camera: Camera3D = %Camera3D
 @onready var _skin: Node3D = %axiaSkin
+
 
 
 #left click to allow mouse camera, esc to exit it
@@ -54,4 +55,4 @@ func _physics_process(delta: float) -> void:
 	if move_direction.length() > 0.2:
 		_last_movement_direction = move_direction
 	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
-	_skin.global_rotation.y = target_angle
+	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
